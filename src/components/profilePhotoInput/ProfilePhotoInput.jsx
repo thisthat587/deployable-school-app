64a1e37c4a7579e49db97175d6661939ccc24a61
Piyush singh <114461687+thisthat587@users.noreply.github.com>
2024-03-16 10:20:47 +0530

    .

--- src/components/profilePhotoInput/ProfilePhotoInput.jsx
+++ src/components/profilePhotoInput/ProfilePhotoInput.jsx
@@ -15,13 +15,14 @@ const ProfilePhotoInput = () => {
             };
 
             reader.readAsDataURL(file);
+            console.log(file);
         }
     };
 
     return (
         <div>
             <label htmlFor="imageInput">
-                <div className='bg-blue-500 flex justify-center items-center'>
+                <div className=' flex justify-center items-center'>
                     <img
                         className="p-2 h-[120px] w-[120px] rounded-full object-cover cursor-pointer"
                         src={imageSrc || 'https://images.unsplash.com/photo-1522199755839-a2bacb67c546?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fGJsb2d8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60'}
